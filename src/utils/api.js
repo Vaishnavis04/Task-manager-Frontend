a84158a40f1bb0c2bd645b31a2358c95397732e0
your-username <vaishnavisakthivel1606@gmail.com>
2025-06-02 09:29:48 +0530

    Add react-router-dom dependency

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://hr-flow-ykzm.onrender.com',
 });
 
 API.interceptors.request.use((config) => {
