085523766a1b9d05a939c72d02c635eb4f1b9833
Vaishnavis04 <136159499+Vaishnavis04@users.noreply.github.com>
2025-06-25 20:19:19 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./signup.css";
+import "./Signup.css";
 import API from "../utils/api";
 
 const Signup = () => {
